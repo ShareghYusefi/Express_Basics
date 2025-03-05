ee964d295b499a1e7810fec9a7b6245cc2de4797
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-03-04 18:18:38 -0700

    server URL's

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // What is express?
 // Express is used to creatre a web server in node.js. Express works on a middleware concept (callback functions).
 
+// import of express module/function
 const express = require("express");
 const app = express();
 
+// localhost:3000/
 app.get("/", function (req, res) {
   res.send("Hello World");
+});
+
+// localhost:3000/hello
+app.get("/hello", function (req, res) {
+  res.send("Hello World 2.0");
 });
 
 app.listen(3000, () => {
