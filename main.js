6c7240a0e6509650ee83a1adf8d6c50dd6a64513
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-03-05 18:55:45 -0700

    Restful API - Get method

--- main.js
+++ main.js
@@ -16,14 +16,56 @@ function customMiddleware(req, res, next) {
 // use the middleware function
 app.use(customMiddleware);
 
-// localhost:3000/
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// What is a Restful API?
+// Restful stands for Representational State Transfer.
+// API stands for Application Programming Interface.
+// A way to design you URL's to interact with the server.
+
+// API's use HTTP methods to interact with the server.
+// GET - To get data from the server.
+// POST - To send data to the server.
+// PATCH - To update data on the server.
+// PUT - To ovveride data objects on the server.
+// DELETE - To delete data from the server.
+
+// HTTP Status Codes
+// These are codes used represent the status of a request.
+// 200 - Success
+// 201 - Created
+// 404 - Not Found
+// 400 - Bad Request
+// 500 - Internal Server Error
+
+// URL stands for Uniform Resource Locator
+// Resource is any type of data that we are storing on the server.
+
+// Mock data (instead of getting data from a database)
+const users = [
+  { id: 1, username: "JohnDoe", email: "JohnDoe@gmail.com" },
+  { id: 2, username: "JaneDoe", email: "JaneDoe@gmail.com " },
+  { id: 3, username: "JasonSmith", email: "JasonSmith@gmail.com" },
+];
+
+// localhost:3000/users
+app.get("/users", function (req, res) {
+  // Mock get data from a database
+  res.status(200).send(users);
 });
 
-// localhost:3000/hello
-app.get("/hello", function (req, res) {
-  res.send("Hello World 2.0");
+// localhost:3000/users/1
+app.get("/users/:id", function (req, res) {
+  // we can get id from query paramaters (query parameters are sent through the URL)
+  let id = parseInt(req.params.id); // convert string to integer
+  // find the user with the id
+  let user = users.find((u) => u.id === id); // (u) => { return u.id === id }  <=====>  (u) => u.id === id
+  // if the use is not found
+  if (!user) {
+    res.status(404).send("User not found");
+    return; // return to stop the function here
+  }
+
+  // if the user is found
+  res.status(200).send(user);
 });
 
 app.listen(3000, () => {
