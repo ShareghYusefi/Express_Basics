b9dfaf5364a9f188d942fd9fad1f0744df064d8a
ShareghYusefi <ShareghYusefi@gmail.com>
2026-07-28 17:01:30 -0600

    Serving JSON Data

--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 // Import http object
 const http = require("node:http");
 
+var menu = [
+  { id: 1, name: "Turkish Coffee", price: 3 },
+  { id: 2, name: "Americano", price: 4 },
+  { id: 3, name: "Iced Latte", price: 5 },
+];
+
 // Create an HTTP tunneling server
 const server = http.createServer(
   // Callback Arrow function (similar to anonymous functions)
   (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
     // check the url
-    if (req.url === "/") {
-      res.end("<h1>Home Page</h1><p>Welcome to our website!</p>");
-    } else if (req.url === "/about") {
-      res.end("<h1>About Page</h1><p>This site was built with Node.js</p>");
+    if (req.url === "/api/menu") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      // JSON.stringify converts arrays and objects to json data format
+      res.end(JSON.stringify(menu));
     } else {
+      res.writeHead(200, { "Content-Type": "text/plain" });
       res.end("<h1>Page Not Found!</h1>");
     }
   },
