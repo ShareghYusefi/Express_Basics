7369b8f6ccf08c1c482e749de4c2dc646ed15289
ShareghYusefi <ShareghYusefi@gmail.com>
2026-07-29 18:49:52 -0600

    Restful API - Patch Method

--- app.js
+++ app.js
@@ -138,6 +138,31 @@ app.post("/users", (req, res) => {
   res.status(201).send(newUser);
 });
 
+// Patch to upadte a user
+app.patch("/users/:id", (req, res) => {
+  // We can find the id from the URL query parameters
+  var id = parseInt(req.params.id);
+  // find the user with matching id
+  var user = users.find((u) => {
+    // predicate/check for a record
+    return u.id === id; // true or false
+  });
+
+  // if user is undefined, we return 404
+  if (!user) {
+    res.status(404).send({
+      message: "User not found.",
+    });
+  }
+
+  // update the user that was found
+  user.username = req.body.username;
+  user.email = req.body.email;
+
+  // default response
+  res.status(200).send(user);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
